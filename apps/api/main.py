c1e27b97c359f78e2f31f3284535c61114980395
Abhishek Jangid <mrabhi748@gmail.com>
2026-08-22 11:58:39 +0530

    chore(api): scalfold fastapi and also configure package.json for monorepo

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from api!")
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+@app.get("/")
+def health():
+    return {"health":True}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000,reload=True)
