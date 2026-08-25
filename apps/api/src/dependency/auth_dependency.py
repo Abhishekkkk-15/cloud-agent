6414b7454630c83b4193b798964eb15a6daf37dd
Abhishek Jangid <mrabhi748@gmail.com>
2026-08-25 10:40:24 +0530

    fix(depenedency): reading both bearer and access tokens

--- apps/api/src/dependency/auth_dependency.py
+++ apps/api/src/dependency/auth_dependency.py
@@ -1,29 +1,43 @@
 from typing import Annotated
-from fastapi import Cookie, Depends, HTTPException, status
+from fastapi import Cookie, Depends, HTTPException, status,Header
 import jwt
 from dotenv import load_dotenv
 from jwt.exceptions import InvalidTokenError
-from src.repository.user import UserRepo
+from src.repository.user import UserRepo,UserRepository
 from src.models.users import User
+from src.repository.user import get_user_repo
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 import os
 load_dotenv()
-
+security = HTTPBearer()
 SECRET_KEY = os.getenv("JWT_SECRET") or "dev-insecure-change-me"
 ALGORITHM = "HS256"
 async def get_current_user(
-    refresh_token: Annotated[str | None, Cookie()] = None,
-    repo = UserRepo 
+     *,
+    ca_refresh_token: Annotated[str | None, Cookie()] = None,
+    ca_access_token: Annotated[str | None, Cookie()] = None,
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    repo:  Annotated[UserRepository, Depends(get_user_repo)],
 ) -> User:
+    print(ca_access_token,credentials)
     
-    if not refresh_token:
+    token = ca_access_token or credentials.credentials 
+    
+    print(token)   
+    if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated. Missing refresh token cookie.",
+            detail="Not authenticated. Missing  token cookie.",
         )
-        
     try:
-        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: str = payload.get("sub")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token payload.",
+            )
+        user_id: str = str(payload.get("sub")) 
         if user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
--- apps/api/src/routes/user.py
+++ apps/api/src/routes/user.py
@@ -7,4 +7,4 @@ from src.controller.user_controller import register_user,read_user_me
 router = APIRouter(prefix="/users",tags=["Users"])
 
 router.post("/register",response_model=RegisterUserReponse,status_code=status.HTTP_201_CREATED)(register_user)
-router.get("/me",response_model=User,status_code=status.HTTP_200_OK)(read_user_me)
+router.get("/me",status_code=status.HTTP_200_OK)(read_user_me)
